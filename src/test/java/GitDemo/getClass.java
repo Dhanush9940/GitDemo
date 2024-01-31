e06094779ffeb6b1164b3587e288722054e52ac3
dhanush9940 <rd1970dha@gmail.com>
2024-01-31 16:38:31 +0530

    This the first Programm

---
+++ src/test/java/GitDemo/getClass.java
@@ -0,0 +1,14 @@
+package GitDemo;
+
+public class getClass {
+
+	public static void main(String[] args) {
+		
+	   System.out.println("Hi Git is pushed");
+	   System.out.println("Hi Git2 is Pushed");
+	   
+		// TODO Auto-generated method stub
+
+	}
+
+}
