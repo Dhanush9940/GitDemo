132db098cf0493b50fa96165e18d7de663592c48
Dhanush9940 <rd1970dha@gmail.com>
2024-02-07 11:09:49 +0530

    Hi nigerians

--- src/test/java/GitDemo/getClass.java
+++ src/test/java/GitDemo/getClass.java
@@ -6,7 +6,7 @@ public class getClass {
 		
 	   System.out.println("Hi Git is pushed");
 	   System.out.println("Hi Git2 is Pushed");
-	   
+	   System.out.println("Hello git54 is pushed");
 		// TODO Auto-generated method stub
 
 	}
